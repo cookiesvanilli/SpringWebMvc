c96d87bf2902da77765362866c3ff325a42ed649
Inna Kuklina <in.d2010@yandex.ru>
2024-10-27 18:20:33 +0300

    Initial commit

---
+++ src/main/java/org/example/service/PostService.java
@@ -0,0 +1,35 @@
+package org.example.service;
+
+import org.example.model.Post;
+import org.example.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import org.example.exception.NotFoundException;
+
+import java.util.Collection;
+
+@Service
+public class PostService {
+
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public Collection<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
